9e9a55dbc1f59b9941b3fd230bf27842373d0259
Korben3 <42899281+Korben3@users.noreply.github.com>
2021-01-30 23:24:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import LiskerViewer from "./LiskerViewer";
+
+const App = () => {
+  return (
+    <div>
+      <LiskerViewer
+        size="250"
+        setLiskerId="t1128t313t416t492t522t535t544t552t701t801t855c1D9EADFc2FF830Cc3F8BB91c48D4731"
+      />
+    </div>
+  );
+};
+
+export default App;
